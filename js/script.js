56b0c90da1e9479b3a32ca4636bcaf220fcbe206
Your Name <e@w.com>
2020-05-24 15:01:38 +0500

    add modal window

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
+    //tabs
         const tabs = document.querySelectorAll('.tabheader__item'),
         tabContents = document.querySelectorAll('.tabcontent'),
         tabsContainer = document.querySelector('.tabheader__items');
@@ -32,6 +33,7 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
   });
+  //timer
   let deadline = '2020-11-10T09:10:00';
 
 function getTimeRemaining(endTime){
@@ -78,8 +80,42 @@ function setTimerValue(id, endTime){
                 timerSeconds.textContent = '00';
             }
     }
-
 }
 setTimerValue('#timer', deadline);
-
+//modal window
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+      function openModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow ='hidden';
+        clearInterval(modalTimerId);
+      }
+      function closeModal(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow ='';
+      }
+      modalTrigger.forEach(el => el.addEventListener('click', openModal));
+      modalCloseBtn.addEventListener('click', closeModal);
+      modal.addEventListener('click', ({target}) => {
+          if(target === modal && modal.classList.contains('show')){
+             closeModal();
+          }
+      });
+      document.addEventListener('keydown', ({code}) =>{
+          if(code === 'Escape'){
+              closeModal();
+          }
+      });
+      const modalTimerId = setTimeout(openModal, 10000);
+      function showModalByScroll(){
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+      }
+      window.addEventListener('scroll', showModalByScroll);
+          
 });
