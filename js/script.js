c874f16b8cc23f8d0dbf0c95ea7d107c6c13726f
Mariya Lubashevskaya <Selestina84@gmail.com>
2020-06-26 18:31:12 +0800

    add functional modal window

--- js/script.js
+++ js/script.js
@@ -117,5 +117,74 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
         }
       }
       window.addEventListener('scroll', showModalByScroll);
-    // fetch form
+
+      // fetch form
+
+      let forms = document.querySelectorAll('form')
+      const message = {
+        loading: 'img/icons/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+      };
+      function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 2000);
+      };
+      forms.forEach(item=>{
+          postData(item)
+      })
+
+      function postData(form){
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+        const statusMessage = document.createElement('img');
+        statusMessage.src = message.loading;
+        statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+        `;
+        form.insertAdjacentElement('afterend', statusMessage);
+
+        const formData = new FormData(form);
+        let object ={id: Math.round(Math.random() * 1e9)};
+        formData.forEach((value, key) => {
+            object[key]=value
+        });
+        fetch('https://my-json-server.typicode.com/Selestina84/Fitnes-menu/users',{
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        })
+        .then (response => response.json())
+        .then(json =>{
+            console.log(json);
+            showThanksModal(message.success);
+            statusMessage.remove();
+        }).catch(()=>{
+            showThanksModal(message.failure);
+        }).finally(()=>{
+            form.reset();
+        })
+
+    })
+ }
 });
