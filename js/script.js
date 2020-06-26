88d0750221dd04ebee280bb7566f930483e2358d
Mariya Lubashevskaya <Selestina84@gmail.com>
2020-06-26 23:12:23 +0800

    add getResourse

--- js/script.js
+++ js/script.js
@@ -159,6 +159,7 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
             },
             body: data
         });
+        return await res.json();
       };
 
       function bindPostData(form){
@@ -187,7 +188,80 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
         }).finally(()=>{
             form.reset();
         })
+      })
+    };
+    //render data
+   /* class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
 
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    };*/
+
+    const getResourse = async (url) => {
+        const res = await fetch(url)
+         if(!res.ok){
+            throw new Error(`Could not fetch ${url} status ${res.status}`)
+        }
+        return await res.json();
+    };
+
+   /* getResourse("https://my-json-server.typicode.com/Selestina84/Fitnes-menu/menu")
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) =>
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+            );
+        })*/
+    getResourse("https://my-json-server.typicode.com/Selestina84/Fitnes-menu/menu")
+    .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) =>{
+            const element = document.createElement('div');
+            element.classList.add("menu__item")
+            element.innerHTML = `
+                <img src=${img} alt=${altimg}>
+                <h3 class="menu__item-subtitle">${title}</h3>
+                <div class="menu__item-descr">${descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                </div>
+            `;
+            document.querySelector('.menu .container').append(element);
+        })
     })
- }
 });
